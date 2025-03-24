6e6580576bbdfab1835f062ecc6b7797bd319804
Patricia Boian <patyboian86@gmail.com>
2025-03-24 14:50:00 -0300

    envio de arquivos das aulas

---
+++ src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for(int carneirinhos = 1; carneirinhos<=20; carneirinhos ++){
+            System.out.println("Contando carneirinhos " + carneirinhos);
+        }
+        // para verificar que o programa sai do loop
+        System.out.println("Joãozinho dormiu");
+    }
+    
+}
